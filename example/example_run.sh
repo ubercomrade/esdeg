a0158c1c0fb88c94e3378d78605061d7e79756f7
Anton Tsukanov <ubercomrade@gmail.com>
2021-11-22 17:43:12 +0700

    fix

--- example/example_run.sh
+++ example/example_run.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 enREST.py matrix \
-./E-GEOD-48230-query-results.tsv \
+./E-GEOD-48230-query-results.csv \
 ./OVOL1_HUMAN.H11MO.0.C.pcm \
 hg38 \
 ./ovol1.enrichment.tsv \
 -m enrichment
 
 enREST.py matrix \
-./E-GEOD-48230-query-results.tsv \
+./E-GEOD-48230-query-results.csv \
 ./OVOL1_HUMAN.H11MO.0.C.pcm \
 hg38 \
 ./ovol1.fraction.tsv \
 -m fraction
 
 enREST.py hocomoco \
-./E-GEOD-48230-query-results.tsv \
+./E-GEOD-48230-query-results.csv \
 ./HOCOMOCOv11_core_pcms_HUMAN_mono_example.txt \
 hg38 \
 ./hocomoco.enrichment.tsv \
 -m enrichment
 
 enREST.py hocomoco \
-./E-GEOD-48230-query-results.tsv \
+./E-GEOD-48230-query-results.csv \
 ./HOCOMOCOv11_core_pcms_HUMAN_mono_example.txt \
 hg38 \
 ./hocomoco.fraction.tsv \
