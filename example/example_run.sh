1289568a2a74a1222eded6888a096f13abe26a97
Anton Tsukanov <ubercomrade@gmail.com>
2022-02-08 20:26:48 +0500

    update example

--- example/example_run.sh
+++ example/example_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+tar -xjvf fasta.tar.bz
+
 time enREST.py deg \
 ./E-GEOD-48230-query-results.csv \
 ./several_matrices_for_deg_testing.txt \
@@ -36,4 +38,26 @@ hg38 \
 ./nfkb1.montecarlo.fraction \
 --parameter fraction \
 --format hocomoco \
+-c 4
+
+
+time enREST.py fasta \
+./foreground.seq \
+./background.seq \
+./tair10.meme \
+tair10 \
+./tair10.montecarlo.enrichment \
+--parameter enrichment \
+--format meme \
+-c 4
+
+
+time enREST.py fasta \
+./foreground.seq \
+./background.seq \
+./tair10.meme \
+tair10 \
+./tair10.montecarlo.fraction \
+--parameter fraction \
+--format meme \
 -c 4
