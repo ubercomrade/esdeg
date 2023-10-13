07f46554a2d194239cc50e070deefaeed2ffb601
Anton Tsukanov <tsukanov@bionet.nsc.ru>
2023-10-13 11:17:36 +0700

    new promoters parser

--- esdeg/parsers.py
+++ esdeg/parsers.py
@@ -9,17 +9,37 @@ def complement(seq):
     return seq.replace('A', 't').replace('T', 'a').replace('C', 'g').replace('G', 'c').upper()[::-1]
 
 
+#OLD
+# def promoters_parser(path):
+#     container = []
+#     gname = ''
+#     with open(path) as file:
+#         for line in file:
+#             if not line.startswith('>'):
+#                 seq = line.strip().upper()
+#                 seq += complement(seq)
+#                 container.append((gname, seq))
+#             else:
+#                 gname = line.strip().split(':')[0][1:]
+#     container.sort(key=itemgetter(0))
+#     promoters_ids = [i[0] for i in container]
+#     promoters = [i[1] for i in container]
+#     return promoters, np.array(promoters_ids)
+
+
 def promoters_parser(path):
     container = []
     gname = ''
     with open(path) as file:
         for line in file:
-            if not line.startswith('>'):
-                seq = line.strip().upper()
-                seq += complement(seq)
-                container.append((gname, seq))
-            else:
+            if line.startswith('>'):
+                if gname != '':
+                    container.append((gname, seq + complement(seq)))
                 gname = line.strip().split(':')[0][1:]
+                seq = ''
+            else:
+                seq += line.strip().upper()
+        container.append((gname, seq + complement(seq)))
     container.sort(key=itemgetter(0))
     promoters_ids = [i[0] for i in container]
     promoters = [i[1] for i in container]
